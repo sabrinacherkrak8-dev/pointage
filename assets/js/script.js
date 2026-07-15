e3147e7c905c3c54f36eac3bfa63671710312555
sabrinacherkrak8-dev <sabrinacherkrak8@gmail.com>
2026-07-15 13:32:38 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -431,7 +431,9 @@ window.deleteEntry = async function(arriveeId,departId){
 };
 
 window.deleteOldEntries = async function () {
-
+    
+     console.log("Fonction appelée");
+    
     const texte = prompt(
         'Tapez "SUPPRIMER" pour effacer les pointages de plus de 7 jours.'
     );
